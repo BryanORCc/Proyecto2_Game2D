fd48188734f5583fb0a3209b27482bc8acebb7e6
BryanORCc <bryanorcc@outlook.com>
2023-07-15 23:25:50 -0500

    add connection to mongoDB

--- Assets/Scripts/script_finalResult.cs
+++ Assets/Scripts/script_finalResult.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using UnityEngine.SceneManagement;
 using TMPro;
+using MongoDB.Driver;
+using MongoDB.Bson;
 
 public class script_finalResult : MonoBehaviour
 {
+    private MongoClient client;
+    private IMongoDatabase database;
+    private IMongoCollection<BsonDocument> collection;
+
     public TextMeshProUGUI totalResult;
     // Start is called before the first frame update
     void Start()
@@ -14,11 +19,32 @@ public class script_finalResult : MonoBehaviour
         float total = script_textControl.instance.nota;
         double percentage = Math.Round((total * 100) / 20, 2);
         totalResult.text = "Puntuación obtenida: " + percentage.ToString() + "%";
+
+        InitConnection();
+        InsertData(percentage.ToString() + "%");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InitConnection()
     {
-        
+        string connectionString = "mongodb+srv://BryanORCc:Rhythm1235@cluster.naaa8iz.mongodb.net/?retryWrites=true&w=majority";
+        client = new MongoClient(connectionString);
+        database = client.GetDatabase("Game");
+
+        // Obtener una referencia a la colección
+        collection = database.GetCollection<BsonDocument>("users");
+    }
+
+    private void InsertData(string note)
+    {
+        string name = PlayerPrefs.GetString("Name");
+        var document = new BsonDocument
+        {
+            {"name", name },
+            { "note", note }
+        };
+
+        collection.InsertOne(document);
+
+        Debug.Log("Documento insertado en la colección.");
     }
 }
